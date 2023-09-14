f8342820998febcdd5068d347ae61970973f0a89
Kshaunish1010 <kkfd614@gmail.com>
2023-09-14 19:57:02 +0530

    some changes

--- LeetCode/ReconstructItinerary.cpp
+++ LeetCode/ReconstructItinerary.cpp
@@ -4,39 +4,44 @@ using namespace std;
     ios_base::sync_with_stdio(false); \
     cin.tie(NULL);
 
-class Solution {
+class Solution
+{
 public:
     vector<string> ans;
-    
-    void dfs(unordered_map<string, vector<string>> &adj, string node) {
-        while (!adj[node].empty()) {
+
+    void dfs(unordered_map<string, vector<string>> &adj, string node)
+    {
+        while (!adj[node].empty())
+        {
             string next_dest = adj[node].back();
             adj[node].pop_back();
             dfs(adj, next_dest);
         }
         ans.push_back(node);
     }
-    
-    vector<string> findItinerary(vector<vector<string>>& tickets) {
+
+    vector<string> findItinerary(vector<vector<string>> &tickets)
+    {
         unordered_map<string, vector<string>> adj;
-        
+
         // Build the adjacency list
-        for (auto& ticket : tickets) {
+        for (auto &ticket : tickets)
+        {
             adj[ticket[0]].push_back(ticket[1]);
         }
-        
+
         // Sort the destinations in lexical order
-        for (auto& pair : adj) {
+        for (auto &pair : adj)
+        {
             sort(pair.second.begin(), pair.second.end(), greater<string>());
         }
-        
+
         dfs(adj, "JFK");
         reverse(ans.begin(), ans.end()); // Reverse to get the correct order
-        
+
         return ans;
     }
 };
-
 
 int main()
 {
